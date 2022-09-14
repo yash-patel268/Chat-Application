3b175bab49a1ff38b03a24d8ad6ffe371c193f71
yash-patel268 <yashp2126@gmail.com>
2022-09-14 14:53:40 -0400

    Added http library to create server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT =  3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
