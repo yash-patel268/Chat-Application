78659bd06b8e45d0efba5b26383bba2c7c8d2ac6
yash-patel268 <yashp2126@gmail.com>
2022-09-14 14:59:36 -0400

    Added socket.io library for communication

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection'); 
+});
 
 const PORT =  3000 || process.env.PORT;
 
