7bb49085b3052e195a557473b86c8904abaf4b63
yash-patel268 <yashp2126@gmail.com>
2022-09-14 15:21:21 -0400

    User message is now saved in server

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ io.on('connection', socket => {
         io.emit('message', 'A user has left the chat');
     });
 
+    //Listen for chatMessage
+    socket.on('chatMessage', () => {
+        io.emit('message', msg);
+    });
 });
 
 const PORT =  3000 || process.env.PORT;
