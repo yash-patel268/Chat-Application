68d0b781a6a4260e7cacaef84be96a467e8fce32
yash-patel268 <yashp2126@gmail.com>
2022-09-15 20:30:19 -0400

    Added formatMesage module and added it to messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,24 @@ const io = socketio(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatApp Bot'
+
 //Run when client connects
 io.on('connection', socket => {
     //Welcome current user
-    socket.emit('message', 'Welcome to ChatApp');
+    socket.emit('message', formatMessage(botName,'Welcome to ChatApp'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
     //Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('User', msg));
     });
 });
 
