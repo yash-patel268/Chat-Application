622e31fec98c23fd463567f910e0eedf42ace6b5
yash-patel268 <yashp2126@gmail.com>
2022-09-17 16:22:02 -0400

    Server now records username with room information

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ io.on('connection', socket => {
         socket.broadcast.to(user.room).emit(
             'message', 
             formatMessage(botName,`${user.username} has joined the chat`));
+        
+        //Send users and room info
+        io.to(user.room).emit('roomUsers', {
+           room: user.room,
+           users: getRoomUsers(user.room) 
+        });
     });
 
     //Listen for chatMessage
@@ -43,6 +49,12 @@ io.on('connection', socket => {
 
         if(user) {
             io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+            //Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room) 
+            });
         }
     });
 });
